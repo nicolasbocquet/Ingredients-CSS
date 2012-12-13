4aff38ef3794e5f1ee5d46af3c0fdb313aa8c27a
Nicolas Bocquet <nicolas@upandnet.com>
2012-12-13 15:21:55 +0100

    Initial Files

---
+++ javascripts/html5-ie.js
@@ -0,0 +1,11 @@
+document.createElement("header");
+document.createElement("hgroup");
+document.createElement("section");
+document.createElement("aside");
+document.createElement("nav");
+document.createElement("article");
+document.createElement("time");
+document.createElement("figure");
+document.createElement("figcaption");
+document.createElement("footer");
+document.createElement("mark");
